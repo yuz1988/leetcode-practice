2090de2f2c4c7f0bb769d91dd825052a80a568e8
炒菜王 <yuz1988@iastate.edu>
2017-05-19 21:10:04 -0500

    two sum

---
+++ leetcode-practice/src/Solution1.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class Solution1 {
+    public int[] twoSum(int[] nums, int target) {
+        int[] indexes = new int[2];
+        if (nums==null || nums.length==0) {
+            return indexes;
+        }
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i=0; i<nums.length; i++) {
+            int diff = target-nums[i];
+            if (map.containsKey(diff)) {
+                indexes[0] = map.get(diff);
+                indexes[1] = i;
+                return indexes;
+            }
+            map.put(nums[i], i);
+        }
+        return indexes;
+    }
+}
