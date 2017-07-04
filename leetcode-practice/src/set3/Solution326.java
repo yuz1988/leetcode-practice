ad8b38014183a4d26c99f39e2cf8f92df057deb3
炒菜王 <yuz1988@iastate.edu>
2017-07-03 22:07:08 -0500

    3^19 (max power of 3 less than Integer.MAX_VALUE)

---
+++ leetcode-practice/src/set3/Solution326.java
@@ -0,0 +1,18 @@
+package set3;
+
+public class Solution326 {
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        
+        while (n>=3) {
+            if (n%3 != 0) {
+                return false;
+            }
+            n = n/3;
+        }
+        
+        return (n==1);    
+    }
+}
