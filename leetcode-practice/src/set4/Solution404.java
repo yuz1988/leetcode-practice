3ec9526541c517891a8366827ab2c71eadca7e76
炒菜王 <yuz1988@iastate.edu>
2017-07-08 01:37:00 -0500

    sum of left leaves

---
+++ leetcode-practice/src/set4/Solution404.java
@@ -0,0 +1,31 @@
+package set4;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution404 {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root==null) {
+            return 0;
+        }
+        
+        int sum = 0;
+        if (root.left != null) {
+            if (root.left.left==null && root.left.right==null) {
+                sum += root.left.val;
+            }
+            else {
+                sum += sumOfLeftLeaves(root.left);
+            }
+        }
+        sum += sumOfLeftLeaves(root.right);
+        return sum;
+        
+    }
+}
---
+++ leetcode-practice/src/set4/TreeNode.java
@@ -0,0 +1,14 @@
+package set4;
+
+/**
+ * Definition for a binary tree node.
+ */
+public class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
