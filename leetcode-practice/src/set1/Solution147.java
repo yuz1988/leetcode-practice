c750e291c7a537fa1b0d1fa60a24e67fa3a038a1
炒菜王 <yuz1988@iastate.edu>
2017-07-31 14:48:09 -0500

    147 should be easy question

---
+++ leetcode-practice/src/set1/Solution147.java
@@ -0,0 +1,31 @@
+package set1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution147 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.empty()) {
+            TreeNode node = stack.pop();
+            if (node != null) {
+                res.add(node.val);
+                stack.push(node.right);
+                stack.push(node.left);
+            }
+        }
+        return res;
+    }
+}
